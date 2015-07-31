bcb87a12035380c5df12cbe9c69e2bb59870db51
kenzmitnick <kenzmitnick@gmail.com>
2015-07-31 17:32:41 +0000

    Creation des classes pour la partie SWING

---
+++ conges/swing/Fenetre.java
@@ -0,0 +1,18 @@
+package conges.swing;
+
+import javax.swing.JFrame;
+
+public class Fenetre extends JFrame {
+
+	public Fenetre () {
+		super();
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Fenetre fn = new Fenetre();
+		fn.show();
+	}
+
+}
